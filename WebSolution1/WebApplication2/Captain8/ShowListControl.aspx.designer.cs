cb2cd673a2a782a564dbb7b85d17a81c430c0c25
Cui-NotePC <jieaido@163.com>
2016-05-31 17:43:50 +0800

    Captain-8 ShowlistControl

--- WebSolution1/WebApplication2/Captain8/ShowListControl.aspx.designer.cs
+++ WebSolution1/WebApplication2/Captain8/ShowListControl.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace WebApplication2.Captain8 {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// BulletedList1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.BulletedList BulletedList1;
+        
+        /// <summary>
+        /// SrcMovies 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SrcMovies;
     }
 }
